af5b5c7ee552cce6edd7c65139a9a7415ceebb5c
Julia Felixa Q. Castaneda <imyourjulie28@gmail.com>
2025-11-17 08:28:55 +0800

    Added if else statement

--- hello.js
+++ hello.js
@@ -1,1 +1,27 @@
-console.log("Hello, World!");
+function legalAge(Age){
+    if (age>= 18){
+        console.log("You are an adult");
+    }else{
+        console.log("You are not an adult");
+    }
+    }
+legalAge(50);
+
+function computerGrade(grade){
+    if(grade >= 94.5){
+        console.log("1");
+    }else if (grade >=91.5){
+        console.log("1.25");
+    }else if(grade >=89.5){
+        console.log("1.5");
+    }else if (grade >=86.5){
+        console.log("1.75");
+    }else if (garde >=84.5){
+        console.log("2");
+    }else{
+        console.log("Not within the transmutation.");
+
+    }
+}
+computerGrade(85);
+    
