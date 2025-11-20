57c3d50ae6240040cff2f2ed447eb384f56449d4
Ms. Julie <imyourjulie28@gmail.com>
2025-11-20 08:33:18 +0800

    Added Commit

---
+++ while-loop.js
@@ -0,0 +1,36 @@
+// console.log("Descending");
+// i = 1;
+
+// while (i >= 1 ){
+//     console.log(i);
+//     i++;
+// }
+// console.log("Ascending");
+// x = 1;
+
+// while (x <= 5){
+//     console.log(x);
+//     x
+// }
+
+// output = " ";
+// x=1;
+
+// while (x<=5){
+//     output += x + " ";
+//     x++;
+// }
+// console.log(output);
+
+arr= [1,2,3];
+output = " ";
+index = 0;
+
+while(index <=arr.length){
+    output += index + " ";
+    inedx++;
+}
+
+console.log(output);
+
+
